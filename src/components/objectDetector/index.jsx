8be6dc340b89bc02a8f42a701ce4e0ff7f60b844
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-02 16:05:28 -0400

    styled index objectDetector

---
+++ src/components/objectDetector/index.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components"
+
+const ObjectDetectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DetectorContainer = styled.div`
+  min-width: 200px;
+  height: 500px;
+  border: 3px solid #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const TargetImg = styled.img`
+  height: 100%;
+`;
+
+const HiddenFileInput = styled.input`
+display: none;
+`
+
+const SelectButton = styled.button`
+  padding: 7px 10px;
+  border: 2px solid transparent;
+  background-color: #fff;
+  color: #0a0f22;
+  font-size: 16px;
+  font-weight: 500;
+  outline: none;
+  margin-top: 2em;
+  curosr: pointer;
+  transition: all 260ms ease-in-out;
+
+  &:hover {
+    background-color: transparent;
+    border: 2px solid #fff;
+    color: #fff;
+}
+`;
+
+const ObjectDetector = (props) => {
+    <div>
+      
+    </div>
+  )
+}
+
+export default ObjectDetector
