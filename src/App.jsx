dca25df66ff73f94646ad2d0eaf560ec495e4f36
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-02 16:51:36 -0400

    loading img

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import styled from 'styled-components';
+import ObjectDetector from './components/objectDetector';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #1c2127;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppContainer>
+      <ObjectDetector />
+    </AppContainer>
+  );
 }
 
-export default App
+export default App;
--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -1,4 +1,8 @@
-import styled from "styled-components"
+import styled from 'styled-components';
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import { useRef, useState } from 'react';
 
 const ObjectDetectorContainer = styled.div`
   display: flex;
@@ -8,7 +12,7 @@ const ObjectDetectorContainer = styled.div`
 
 const DetectorContainer = styled.div`
   min-width: 200px;
-  height: 500px;
+  height: 700px;
   border: 3px solid #fff;
   border-radius: 5px;
   display: flex;
@@ -22,8 +26,8 @@ const TargetImg = styled.img`
 `;
 
 const HiddenFileInput = styled.input`
-display: none;
-`
+  display: none;
+`;
 
 const SelectButton = styled.button`
   padding: 7px 10px;
@@ -34,21 +38,52 @@ const SelectButton = styled.button`
   font-weight: 500;
   outline: none;
   margin-top: 2em;
-  curosr: pointer;
+  cursor: pointer;
   transition: all 260ms ease-in-out;
 
   &:hover {
     background-color: transparent;
     border: 2px solid #fff;
     color: #fff;
-}
+  }
 `;
 
-const ObjectDetector = (props) => {
-    <div>
-      
-    </div>
-  )
+function ObjectDetector(props) {
+  const fileInputRef = useRef();
+  const [imgData, setImgData] = useState(null)
+
+  const openFilePicker = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  const readImage = (file) => {
+    return new Promise((rs, rj) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => rs(fileReader.result);
+      fileReader.onerror = () => rj(fileReader.error);
+      fileReader.readAsDataURL(file);
+    });
+  };
+
+  const onSelectImage = async (e) => {
+    const file = e.target.files[0];
+    const imgData = await readImage(file);
+    setImgData(imgData)
+  };
+
+  return (
+    <ObjectDetectorContainer>
+      <DetectorContainer>
+        {imgData && <TargetImg src={imgData} />}
+      </DetectorContainer>
+      <HiddenFileInput
+        type="file"
+        ref={fileInputRef}
+        onChange={onSelectImage}
+      />
+      <SelectButton onClick={openFilePicker}>Select Image</SelectButton>
+    </ObjectDetectorContainer>
+  );
 }
 
-export default ObjectDetector
+export default ObjectDetector;
